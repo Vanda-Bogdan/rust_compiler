ea9cd336f0a23153be6003afc1d12a5d2b32c8e2
Lim <andrey.sidor.01@mail.ru>
2023-01-25 14:26:55 +0300

    Добавлены функции перевода чисел в массивы байт

--- RustCompiller/src/main/generation/utils/Utils.java
+++ RustCompiller/src/main/generation/utils/Utils.java
@@ -1,11 +1,42 @@
 package main.generation.utils;
 
+import main.semantic.*;
+
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 public class Utils {
 
-//    public static byte[] to2ByteArray() {
-//        return
-//    }
+    // ---------------------------Перевод численных значений в массив байт и обратно---------------------- //
+    public static byte[] intTo2ByteArray(int value) {
+        return new byte[] {
+                (byte)(value >> 8),
+                (byte)value};
+    }
+
+    public static byte[] intTo4ByteArray(int value) {
+        return ByteBuffer.allocate(4).putInt(value).array();
+    }
+
+    public static byte[] floatTo4ByteArray(float value) {
+        return ByteBuffer.allocate(4).putFloat(value).array();
+    }
+
+    public static int intFromByteArray(byte[] arr) {
+        int value = 0;
+        for (byte b : arr) {
+            value = (value << 8) + (b & 0xFF);
+        }
+        return value;
+    }
+
+    public static float floatFromByteArray(byte[] arr) {
+        return ByteBuffer.wrap(arr).getFloat();
+    }
+
+    // -----------------------------------Перевод таблицы констант в массив байт--------------------------------------//
+    public static byte[] constantTableToByteArray(ArrayList<ConstantTable.ConstantTableItem> tableItems) {
+        return null;
+    }
 
 }
