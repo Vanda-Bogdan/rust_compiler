f01288acd6e24f01e6c53339ccec0734a431548e
Vanda Bogdan <bodiavanda@gmail.com>
2023-01-30 16:29:47 +0300

    Функция для exprList

--- RustCompiller/src/main/nodes/expression/ExpressionListNode.java
+++ RustCompiller/src/main/nodes/expression/ExpressionListNode.java
@@ -1,8 +1,18 @@
 package main.nodes.expression;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ExpressionListNode {
     public int id;
     public ArrayList<ExpressionNode> list = new ArrayList<>();
+
+    public ExpressionNode getStructFieldExprByName(String name){
+        for (ExpressionNode expr: list) {
+            if(Objects.equals(expr.name, name)){
+                return expr;
+            }
+        }
+        return null;
+    }
 }
